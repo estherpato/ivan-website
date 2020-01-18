da5e9c499e0ded1ac8048bd0992c37c19e829cc3
estherpato <estherpato@gmail.com>
2020-01-18 18:56:24 +0100

    Fix build issue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   menuOpen = false;
   speed = 100;
+  onHover: boolean = false;
 
   constructor() { }
 
